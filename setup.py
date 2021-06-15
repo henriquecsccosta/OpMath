ff7cb7aca2e078f6e917e010c029028ab842a797
TheGodTeacher <henriquecsccosta@gmail.com>
2021-06-15 16:25:53 +0100

    OpMath

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import os
+requirementPath = "requirements.txt"
+install_requires = []
+
+if os.path.isfile(requirementPath):
+      with open(requirementPath) as f:
+            install_requires = f.read().splitlines()
 
 from setuptools import setup, find_packages
 setup(
@@ -6,5 +12,12 @@ setup(
       version = "0.1",
       description = "OpMath Distribution Package",
       packages = find_packages(),
-      include_packages_data = True
-     )
+      include_packages_data = True,
+      install_requires = install.requires,
+      entry_points = {
+            "console_scripts":[
+                  "OpMath = OpMath.cli: main"
+            ]
+      }
+
+)
