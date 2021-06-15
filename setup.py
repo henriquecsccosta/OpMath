1126ec3cb6a4c23b3eace49a2bc15573fbc63a14
TheGodTeacher <henriquecsccosta@gmail.com>
2021-06-15 14:44:53 +0100

    OpMath

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+
+from setuptools import setup, find_packages
+setup(
+      name = "OpMath"
+      version = "0.1"
+      description = "OpMath Distribution Package"
+      packages = find_packages(),
+      include_packages_data = True
+     )
